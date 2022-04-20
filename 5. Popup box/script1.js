0bf27f7fdfdfaeb7e0784275d1fc63bfe8d5924f
raihanph21 <m.rhn.ph@gmail.com>
2022-04-20 23:25:33 +0700

    Buat folder baru popup box, buat file index.html, buat file script1.js dan isinya di masing-masing file

---
+++ 5. Popup box/script1.js
@@ -0,0 +1,24 @@
+// alert('Hello World!');
+
+// var nim = prompt('Masukkan NIM anda: ');
+// var nama = prompt('Masukkan nama: ')
+// alert(nim + ' ' + nama);
+
+/* var coba = confirm("Anda yakin?");
+if (coba === true) {
+    alert('User menekan OK!')
+} else {
+    alert('User menekan CANCEL!')
+} */
+
+alert('selamar datang!')
+var lagi = true;
+
+while (lagi) {
+    var nama = prompt('Masukkan nama anda: ');
+    alert (nama);
+
+    lagi = confirm('coba lagi?');
+}
+
+alert('terimakasih!');
