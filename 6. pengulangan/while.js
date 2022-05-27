9e60859881e98284fb5f92fc1c2601f4687ca2f5
raihanph21 <m.rhn.ph@gmail.com>
2022-05-27 21:42:54 +0700

    buat folder pengulangan, file index.html, dan while.js

---
+++ 6. pengulangan/while.js
@@ -0,0 +1,19 @@
+// var lagi = true;
+// while(lagi) {
+//     console.log("hello world!");
+//     lagi = confirm("lagi?");
+// }
+
+// var awal = 1;
+// while(awal <= 5) {
+//     console.log("hello world!");
+// awal++;
+// }
+
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 10) {
+//     console.log("hello world " + nilaiAwal + "x");
+// nilaiAwal++;
+// }
+
+
