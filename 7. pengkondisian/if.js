5df2534af8d1d242855a9bab30ee835e43b186eb
raihanph21 <m.rhn.ph@gmail.com>
2022-05-28 11:54:43 +0700

    buat file angkot4.js di folder latihan dan ganti dikit di if.js dan index.html di folde pengkondisian

--- 7. pengkondisian/if.js
+++ 7. pengkondisian/if.js
@@ -1,7 +1,7 @@
 var angka = prompt("Masukkan angka: ");
-if (angka % 2 == 0) {
+if (angka % 2 === 0) {
     alert(angka + " merupakan bilangan genap");
-} else if (angka % 2 == 1){
+} else if (angka % 2 === 1){
     alert(angka + " merupakan bilangan ganjil");
 } else {
     alert(angka + " bukan bilangan");
