a64cbe5325c4653bcb948edaca408d2bd057048b
raihanph21 <m.rhn.ph@gmail.com>
2022-05-28 11:28:02 +0700

    mengganti isi file angkot3.js karna salah

--- 6. pengulangan/latihan/angkot3.js
+++ 6. pengulangan/latihan/angkot3.js
@@ -2,10 +2,12 @@ var jmlAngkot = 10;
 var angkotBeroperasi = 6;
 var angkot = 1;
 
-for (angkot; angkot <= angkotBeroperasi; angkot++) {
-    if(angkot <= 6) {
+for (angkot; angkot <= jmlAngkot; angkot++) { 
+    if(angkot <= 6) { 
         console.log("angkot No. " + angkot + " beroperasi dengan baik");
-    } else if (angkot >= 7 ) {
+    } else {
         console.log("Angkot No. " + angkot + " sedang tidak beroperasi")
     }
 }
+
+// bisa juga for(var angkot = 1; ... ; .... .)
