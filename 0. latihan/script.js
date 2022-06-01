df7de28df74dd3f6fb740679bab41f89f1d1af53
raihanph21 <m.rhn.ph@gmail.com>
2022-06-01 09:50:59 +0700

    membuat folder latihan, file index.html dan script.js untuk latihan

---
+++ 0. latihan/script.js
@@ -0,0 +1,18 @@
+// var s = '';
+// for (var i = 0; i < 5; i++) {
+//     for(var j = 0; j <= i; j++) {
+//         s += "*"
+//     }
+//     s += "\n"; // s += "*" -> sama dengan s = s + "*";
+// }
+// console.log(s); 
+
+
+var s = '';
+for (var i = 10; i > 0; i--) {
+    for(var j = 0; j < i; j++) {
+        s += "*"
+    }
+    s += "\n"; // s += "*" -> sama dengan s = s + "*";
+}
+console.log(s); 
