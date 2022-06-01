ee733d587b16d7a31f0a14fdca10cebc505a0c9a
raihanph21 <m.rhn.ph@gmail.com>
2022-06-01 10:47:42 +0700

    mengubah isi script.js

--- 0. latihan/script.js
+++ 0. latihan/script.js
@@ -8,6 +8,15 @@
 // console.log(s); 
 
 
+// var s = '';
+// for (var i = 10; i > 0; i--) {
+//     for(var j = 0; j < i; j++) {
+//         s += "*"
+//     }
+//     s += "\n"; // s += "*" -> sama dengan s = s + "*";
+// }
+// console.log(s); 
+
 var s = '';
 for (var i = 10; i > 0; i--) {
     for(var j = 0; j < i; j++) {
