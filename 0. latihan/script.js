a48f5f0a84a2e32dc2130316f9528f7f42f56085
raihanph21 <m.rhn.ph@gmail.com>
2022-06-04 23:21:37 +0700

    menambah line di script.js di folder 0.latihan

--- 0. latihan/script.js
+++ 0. latihan/script.js
@@ -17,11 +17,24 @@
 // }
 // console.log(s); 
 
-var s = '';
-for (var i = 10; i > 0; i--) {
-    for(var j = 0; j < i; j++) {
-        s += "*"
+// var s = '';
+// for (var i = 10; i > 0; i--) {
+//     for(var j = 0; j < i; j++) {
+//         s += "*"
+//     }
+//     s += "\n"; // s += "*" -> sama dengan s = s + "*";
+// }
+// console.log(s); 
+
+var s = "";
+for( i = 1; i <= 10; i++ ) {
+    for( j = 1; j <= 10; j++ ) {
+        if( i <= j ) {
+            s += "*";          
+        } else {
+            s += " ";
+        }
     }
-    s += "\n"; // s += "*" -> sama dengan s = s + "*";
+    s += "\n";
 }
-console.log(s); 
+console.log(s);
