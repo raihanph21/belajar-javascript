1bda939ce6f35095f50d2ad4d5b63100c250d85c
raihanph21 <m.rhn.ph@gmail.com>
2022-06-11 15:00:20 +0700

    jadiin segitiga campur di script.js

--- 0. latihan/script.js
+++ 0. latihan/script.js
@@ -1,3 +1,4 @@
+// SEGITIGA SIKU" KE KANAN
 // var s = '';
 // for (var i = 0; i < 5; i++) {
 //     for(var j = 0; j <= i; j++) {
@@ -8,6 +9,7 @@
 // console.log(s); 
 
 
+//SEGITIGA CAMPUR YG BAGIAN BAWAH
 // var s = '';
 // for (var i = 10; i > 0; i--) {
 //     for(var j = 0; j < i; j++) {
@@ -17,8 +19,32 @@
 // }
 // console.log(s); 
 
+//SEGITIGA SIKU" DARI KANAN ATAS
+// var s = "";
+// for( i = 1; i <= 10; i++ ) {
+//     for( j = 1; j <= 10; j++ ) {
+//         if( i <= j ) {
+//             s += "*";          
+//         } else {
+//             s += " ";
+//         }
+//     }
+//     s += "\n";
+// }
+// console.log(s);
+
+
+
+// SEGITIGA YANG CAMPUR
 // var s = '';
-// for (var i = 10; i > 0; i--) {
+// for (var i = 0; i < 10; i++) {
+//     for(var j = 0; j <= i; j++) {
+//         s += "*"
+//     }
+//     s += "\n"; // s += "*" -> sama dengan s = s + "*";
+   
+// }
+// for (var i = 9; i > 0; i--) {
 //     for(var j = 0; j < i; j++) {
 //         s += "*"
 //     }
@@ -26,15 +52,4 @@
 // }
 // console.log(s); 
 
-var s = "";
-for( i = 1; i <= 10; i++ ) {
-    for( j = 1; j <= 10; j++ ) {
-        if( i <= j ) {
-            s += "*";          
-        } else {
-            s += " ";
-        }
-    }
-    s += "\n";
-}
-console.log(s);
+//SEGITIGA SAMAKAKI
